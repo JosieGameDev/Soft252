9e15de5c7c17307082f89510d810b9dea0a60500
(s) Josie Wood <josie.wood@students.plymouth.ac.uk>
2019-10-02 12:14:20 +0100

    add methods, getters and setter to bank example

--- Week01/BankExample1/src/bankexample1/BankAccount.java
+++ Week01/BankExample1/src/bankexample1/BankAccount.java
@@ -10,5 +10,41 @@ package bankexample1;
  * @author jwood20
  */
 public class BankAccount {
+    private double balance;
+    private double overdraft;
+    private String holder;
+    
+    public BankAccount(String holder)
+    {
+        this.holder = holder;
+        this.overdraft = 500;
+        this.balance = 100;
+    }
+    public void depositMoney(int amount)
+    {
+        balance += amount;
+    }
+    public boolean withdrawMoney(int amount)
+    {
+        if((balance + overdraft) < amount)
+            return false;
+        else
+        {
+            balance -= amount;
+            return true;
+        }           
+    }
+    public double getBalance()
+    {
+        return balance;
+    }
+    public String getHolder()
+    {
+        return holder;
+    }
+    public void setOverdraft(int newOverdraft)
+    {
+        this.overdraft = newOverdraft;
+    }
     
 }
