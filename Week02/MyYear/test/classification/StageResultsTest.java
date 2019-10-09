b8c9b939de0bbcfd502901c9765aa48d2d7fb15c
(s) Josie Wood <josie.wood@students.plymouth.ac.uk>
2019-10-09 11:45:46 +0100

    added testing to the iscomplete function

---
+++ Week02/MyYear/test/classification/StageResultsTest.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classification;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jwood20
+ */
+public class StageResultsTest {
+    
+ private StageResults empty; // will have no credits and no marks
+ private StageResults full; // will have 120 credits and marks
+ private StageResults halfFull; // will have 60 credits and some marks
+
+    
+    public StageResultsTest() 
+    {
+        
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() 
+    {
+        // empty - object that starts with default values
+        empty = new StageResults();
+
+        // full - object with 120 credits-worth of marks but no
+        // initial stage2Average
+        full = new StageResults();
+        full.addModuleMark(120, 50.0);
+
+        // halfFull - object with 60 credits worth of marks and
+        // no initial stage2Average
+        halfFull = new StageResults();
+        halfFull.addModuleMark(60, 50.0);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testIsComplte()
+    {
+        //fail("Test not yet implemented");
+        System.out.println("Testing is Complete");
+        
+        //check that empty obkect is 'not complete'
+        assertFalse("empty object", empty.isComplete());
+        assertFalse("halfFull object", halfFull.isComplete());
+        assertTrue("full object", full.isComplete());
+    }
+    
+    @Test
+    public void testResetValues()
+    {
+         fail("Test not yet implemented");
+    }
+    
+    @Test
+    public void testAddModuleMark()
+    {
+         fail("Test not yet implemented");
+    }
+    
+    @Test
+    public void testCalculateAverageSoFar()
+    {
+         fail("Test not yet implemented");
+    }
+    
+    @Test
+    public void testPredictClass()
+    {
+         fail("Test not yet implemented");
+    }
+    
+}
